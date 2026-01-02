bb7d7d8baf9924fdf702d7fe980578e5d5f9f608
MParsa9011 <mpsalehi90@gmail.com>
2026-01-02 15:28:32 +0330

    add product image field

--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     # path('grappelli/', include('grappelli.urls')),
 
@@ -25,4 +28,4 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('rosetta/', include('rosetta.urls')),
     path('cart/', include('cart.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
